e816cb1caa83deb2d720714a83a6761f490a517a
Krzysztof Mroz <kmr@trackman.com>
2022-11-14 22:41:45 +0100

    Getting angles from position

---
+++ pythonScript/generate_mesh.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import numpy
+import sys
+import json
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# (1-x^2-y^2)/500=z-10
+# z = 1/500 (-x^2 - y^2 + 5001)
+
+def main():
+    step = 0.5
+    MIN = -50
+    MAX = 50
+    mesh_dict = []
+
+    for x in numpy.arange(MIN, MAX, step):
+        for y in numpy.arange(MIN, MAX, step):
+            z = 1/500 *(-x*x - y*y + 5001)
+            mesh_dict.append({"x": x, "y": y, "z":z})
+
+    json_object = json.dumps(mesh_dict, indent = 4) 
+    f = open("map_grid.json", "x")
+    f.write(json_object)
+    f.close()
+
+if __name__ == "__main__":
+    main()
--- pythonScript/hostApp.py
+++ pythonScript/hostApp.py
@@ -5,36 +5,94 @@ import json
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+STEP = 0.5
+MIN = -50
+MAX = 50
+
+
 def getXYZ(data):
     x = []
     y = []
     z = []
-    for point in data["pointsX"]:
-        x.append(point["x"])
-        y.append(point["y"])
-        z.append(point["z"])
-    
-    for point in data["pointsY"]:
+    for point in data:
         x.append(point["x"])
         y.append(point["y"])
         z.append(point["z"])
     return [x, y, z]
-    
+
+
+def print_plot(x, y, z, pos_x, pos_y, pos_z):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(x, y, z, zdir='z', c='green')
+    ax.scatter(pos_x, pos_y, pos_z, zdir='z', c='red', s=1000)
+    plt.show()
+
+
+def find_index(pos_x, pos_y, x_arr, y_arr):
+    i = x_arr.index(pos_x)
+    for i in range(i, len(x_arr)):
+        if y_arr[i] == pos_y:
+            return i
+    sys.exit(-1)
+
+
+def find_neighbours(x_arr, y_arr, z_arr, matching_index):
+    row_size = int(round((MAX - MIN) / STEP))
+    neighbours = {
+        "left_neighbour": {
+        "x": x_arr[matching_index - row_size],
+        "y": y_arr[matching_index - row_size],
+        "z": z_arr[matching_index - row_size]
+    },
+        "right_neighbour": {
+        "x": x_arr[matching_index + row_size],
+        "y": y_arr[matching_index + row_size],
+        "z": z_arr[matching_index + row_size]
+    },
+        "front_neighbour": {
+        "x": x_arr[matching_index + 1],
+        "y": y_arr[matching_index + 1],
+        "z": z_arr[matching_index + 1]
+    },
+        "back_neighbour": {
+        "x": x_arr[matching_index - 1],
+        "y": y_arr[matching_index - 1],
+        "z": z_arr[matching_index - 1]
+    }}
+    return neighbours
+
+def get_angles(nabo):
+    ln = nabo["left_neighbour"]
+    rn = nabo["right_neighbour"]
+    fn = nabo["front_neighbour"]
+    bn = nabo["back_neighbour"]
+
+    pitch = numpy.rad2deg(numpy.arctan( (fn["z"] - bn["z"]) / (STEP * 2)) )
+    roll = numpy.rad2deg(numpy.arctan( (ln["z"] - rn["z"]) / (STEP * 2)))
+    return [pitch, roll]
+
+
 
 def main(argv):
     if ("help" in argv[0]):
         print("Call program with following order:")
-        print("python hostApp.py <input_file_path>")
+        print("python hostApp.py <map_grid_file> <point_x_coordinate> <point_y_coordinate>")
     else:
         f = open(argv[0])
         data = json.load(f)
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
         [x, y, z] = getXYZ(data)
-        ax.scatter(x, z, y, zdir='z', c= 'red')
 
-        plt.show()
-
+        pos_x = round(float(argv[1]) * 2) / 2
+        pos_y = round(float(argv[2]) * 2) / 2
+        matching_index = find_index(pos_x, pos_y, x, y)
+        pos_z = z[matching_index]
+        print(pos_x, pos_y, pos_z)
+        # print_plot(x, y, z, pos_x, pos_y, pos_z)
+        neighbours = find_neighbours(x, y, z, matching_index)
+        print(neighbours)
+        angles = get_angles(neighbours)
+        print(angles)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
